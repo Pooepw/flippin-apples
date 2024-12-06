cbb858aa3eb490b77d992d666485b3fc0ce6e15b
Mark Grubbs <siegbalicula@gmail.com>
2024-12-05 22:56:26 -0600

    set up sound buses so that they can be muted and changed in sound level. UI needs set up in order to change it.

--- CollaborationGame/collaboration-game/Systems/audio_controller.gd
+++ CollaborationGame/collaboration-game/Systems/audio_controller.gd
@@ -1,11 +1,16 @@
 extends Node
 
+var audio_bus
+var sound_bus
+var music_bus
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	sound_bus = AudioServer.get_bus_index("Sounds")
+	music_bus = AudioServer.get_bus_index("Music")
 
+func set_sound_level(bus_index, db_level):
+	AudioServer.set_bus_volume_db(bus_index, db_level)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func toggle_mute_bus(bus_index):
+	AudioServer.set_bus_mute(bus_index, not AudioServer.is_bus_mute(bus_index))
