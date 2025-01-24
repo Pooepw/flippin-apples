fcd83d5884d92d58f1c8c3c4670524c6cc3484fa
Mark Grubbs <siegbalicula@gmail.com>
2025-01-23 19:11:21 -0600

    put together untested code of attack states (animations) for wand and hammer. still thinking about how to do staff. untested = no test weapon fyi

--- CollaborationGame/collaboration-game/Weapons/WeaponTemplates/hammer_melee_weapon.gd
+++ CollaborationGame/collaboration-game/Weapons/WeaponTemplates/hammer_melee_weapon.gd
@@ -3,16 +3,20 @@ extends melee_weapon
 class_name hammer
 
 @export var charging_time: float
+@export var cooldown_time: float
 @export var reach: int
 
 var strike_area
 var shockwave_area
+var attack_states
 
 var strike_active = false
 var shockwave_active = false
 
 var charging = false
+var can_charge = true
 var charge_timer
+var cooldown_timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,26 +26,37 @@ func _ready() -> void:
 	shockwave_area = get_node("ShockwaveArea")
 	shockwave_area.monitoring = false
 	charge_timer = get_node("ChargeTimer")
+	attack_states = get_node("AttackStates")
+	cooldown_timer = get_node("CooldownTimer")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if attacking:
-		if not charging: 
+		if not charging and can_charge: 
 			charging = true
 			charge_timer.start(charging_time)
-		
+			attack_states.frame = 1
 
 func start_attack():
 	attacking = true
 
 func end_attack():
 	attacking = false
+	if charging:
+		charging = false
+		attack_states.frame = 0
 
 func _on_charge_timer_timeout_do_attack():
+	attack_states.frame = 2
 	var to_mouse = position.direction_to(get_global_mouse_position())
 	var damage_position = position + to_mouse * reach
 	strike_area.position = damage_position
 	shockwave_area.position = damage_position
-	# need animation here
 	
+	cooldown_timer.start(cooldown_time)
+
+func _on_cooldown_timer_timeout() -> void:
+	can_charge = true
 	charging = false
+	if attacking:
+		attack_states.frame = 1
--- CollaborationGame/collaboration-game/Weapons/WeaponTemplates/wand_magic_weapon.gd
+++ CollaborationGame/collaboration-game/Weapons/WeaponTemplates/wand_magic_weapon.gd
@@ -3,23 +3,31 @@ extends magic_weapon
 class_name wand
 
 @export var projectile_scene: String
-var projectile_node
 
+var projectile_node
+var attack_states
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super()
 	projectile_node = load(projectile_scene)
+	attack_states = get_node("AttackStates")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
 	if casting:
+		attack_states.frame = 1
 		if not cast_started:
 			cast_started = true
-			cast_timer.start()
-			
+			cast_timer.start(cast_time)
+	elif not casting:
+		attack_states.frame = 0
+		if cast_started:
+			cast_timer.stop()
+			cast_started = false
+
 
 func _on_cast_timer_timeout() -> void:
 	Player.current_mana -= mana_cost
