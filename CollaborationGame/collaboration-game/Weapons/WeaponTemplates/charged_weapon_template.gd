0e3a2a9e4cc7e4b5b13068b84217201b75d0aed8
Mark Grubbs <siegbalicula@gmail.com>
2025-08-07 22:57:06 -0500

    rewrite of the gun weapon is finished and 2 of the weapons have been implemented. similar implemenatation to the wand weapon, except with a recharge animation

--- CollaborationGame/collaboration-game/Weapons/WeaponTemplates/charged_weapon_template.gd
+++ CollaborationGame/collaboration-game/Weapons/WeaponTemplates/charged_weapon_template.gd
@@ -2,14 +2,8 @@ extends weapon2
 
 class_name charging_weapon
 
-@export var uses_timer: bool
-@export var charge_time: float
-
 # to be played when charging hammer/bow/staff or reloading gun
 var charging_sprite_frames
-
-# if a weapon uses the timer (probably reloading a gun), then this will be set
-var charging_timer
 
 var charged = false
 
@@ -17,8 +11,6 @@ var charged = false
 func _ready() -> void:
 	super()
 	charging_sprite_frames = weapon_sprites.sprite_frames
-	if uses_timer: 
-		charging_timer = get_node("ChargeTimer")
 
 func handle_charge():
 	var charge = 0
--- CollaborationGame/collaboration-game/Weapons/WeaponTemplates/gun_template.gd
+++ CollaborationGame/collaboration-game/Weapons/WeaponTemplates/gun_template.gd
@@ -2,7 +2,46 @@ extends charging_weapon
 
 class_name gun_type
 
-# maybe unnecessary
-@export var fire_time: float
+@export var mag_size: int
 
-var fire_timer
+var current_bullets
+
+func _ready():
+	super()
+	current_bullets = mag_size
+
+func start_attack():
+	firing = true
+	if weapon_sprites.get_animation() == "Idle":
+		weapon_sprites.play("Firing")
+
+func end_attack():
+	firing = false
+	if weapon_sprites.get_animation() == "Firing":
+		weapon_sprites.stop()
+		weapon_sprites.play("Idle")
+	if current_bullets == 0:
+		weapon_sprites.play("Charging")
+
+func reload():
+	current_bullets = mag_size
+	if firing:
+		weapon_sprites.play("Firing")
+	else:
+		weapon_sprites.play("Idle")
+
+func _on_weapon_animation_finished():
+	if weapon_sprites.get_animation() == "Firing":
+		print("firing")
+		if current_bullets > 0:
+			emit_attack()
+			weapon_sprites.play("Firing")
+		else:
+			begin_charge()
+	elif weapon_sprites.get_animation() == "Charging":
+		reload()
+
+func emit_attack():
+	current_bullets -= 1
+	var bullet_instance = weapon_emission.instantiate()
+	bullet_instance.fire_self(get_global_mouse_position(), get_parent(), damage)
