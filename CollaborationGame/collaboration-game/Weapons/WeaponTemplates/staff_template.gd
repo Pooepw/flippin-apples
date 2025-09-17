5e5201fbfe169210fef030c0255ee6691d7bc691
Mark Grubbs <siegbalicula@gmail.com>
2025-09-16 22:48:37 -0500

    made assets for icons and attacks of staves

--- CollaborationGame/collaboration-game/Weapons/WeaponTemplates/staff_template.gd
+++ CollaborationGame/collaboration-game/Weapons/WeaponTemplates/staff_template.gd
@@ -1,3 +1,19 @@
 extends charging_weapon
 
 class_name staff_type
+
+@export var mana_cost: int
+
+func start_attack():
+	if PlayerHandler.current_player.current_mana >= mana_cost:
+		super()
+
+func emit_attack():
+	PlayerHandler.current_player.current_mana -= mana_cost
+	PlayerHandler.current_player.start_regen_wait("Mana")
+	do_attack()
+
+func do_attack():
+	## insert the attack of the specific weapon
+	## (you have to extend this script)
+	pass
